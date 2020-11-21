a3fb276d105d29f16c3e18c470cb8f44cf28d5b2
Jens Møller <jens@jensz12.com>
2020-11-21 23:28:09 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,251 +1,131 @@
 <!DOCTYPE html>
-
 <html lang="da">
-
 <head>
-
 <meta charset="utf-8" />
-
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
-
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 <title>Forside - Jens Møller</title>
-
 <link rel="icon" href="https://jensz12.com/favicon.png">
-
 <meta name="description" content="Forside - Jens Møller">
-
 <meta name="theme-color" content="#A81010">
-
 <meta name="twitter:card" content="summary_large_image">
-
 <meta name="twitter:site" content="@jensz12">
-
 <meta name="twitter:creator" content="@jensz12">
-
 <meta name="twitter:title" content="Forside - Jens Møller">
-
 <meta name="twitter:description" content="Forside - Jens Møller">
-
 <meta name="twitter:image:src" content="https://www.gravatar.com/avatar/5ce60652703ef30780a3cbb1c0eb0317?s=2000">
-
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
 <link rel="manifest" href="/manifest.json">
-
 <link rel="image_src" href="/https://www.gravatar.com/avatar/5ce60652703ef30780a3cbb1c0eb0317?s=2000">
-
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
-
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-LRlmVvLKVApDVGuspQFnRQJjkv0P7/YFrw84YYQtmYG4nK8c+M+NlmYDCv0rKWpG" crossorigin="anonymous">
-
 <style>
-
 body {
-
-	background-image: url(https://design.jensz12.com/images/screen-back.png);	background-repeat: no-repeat;
-
+	background-image: url(https://design.jensz12.com/images/screen-back.png);
+	background-repeat: no-repeat;
 	background-size: cover;
-
 	background-position: center center;
-
 	background-attachment: fixed;
-
 	height: 100%;
-
 	font-family: 'Roboto', sans-serif;
-
 	padding-top: 35px;
-
 }
-
 .fa-heart {
-
 	color: #A81010;
-
  margin-top: 10px;
-
 }
-
 .fa-twitter {
-
   color: #1DA1F2;
-
   margin: 5px;
-
   }
-
 .fa-discord {
-
   color: #7289DA;
-
   margin: 5px;
-
 }
-
 .fa-instagram {
-
   color: #E1306C;
-
   margin: 5px;
-
 }
-
 .fa-snapchat {
-
   color: #FFFC00;
-
   margin: 5px;
-
 }
-
 .img {
-
     margin : 10px;
-
     margin-bottom: 40px;
-
 }
 
 @media (prefers-color-scheme: light) {
-
   
-
   .card-title, h1, h2, h3, h4, h5, p, b, card-text, a:link, .fa-envelope, a:visited, th, tr {
-
     color: black;
-
   }
-
   .jumbotron, .card {
-
     background-color: white;
-
     margin-bottom: 30px;
-
     margin-top: 30px;
-
   }
-
     .fa-github {
-
     color: black;
-
     margin: 5px;
-
 }
 
 }
 
 @media (prefers-color-scheme: dark) {
-
   
-
   .card-title, h1, h2, h3, h4, h5, p, b, card-text, a:link, .fa-envelope, a:visited, th, tr {
-
     color: white;
-
   }
-
   .jumbotron, .card {
-
     background-color: #212121;
-
     margin-bottom: 30px;
-
     margin-top: 30px;
-
   }
-
   .fa-github {
-
   color: white;
-
   margin: 5px;
-
 }
 
 }
-
 </style>
-
 <script>
-
 	if ('serviceWorker' in navigator) {
-
   window.addEventListener('load', function() {
-
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-
       // Registration was successful
-
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-
     }).catch(function(err) {
-
       // registration failed :(
-
       console.log('ServiceWorker registration failed: ', err);
-
     });
-
   });
-
 }
-
 </script>
-
 </head>
-
 <body>
-
 <div class="container">
-
   <div class="row">
-
     <div class="col-lg-12 col-md-12">
-
       <div class="jumbotron">
-
         <h1 class="display-1 text-center">Jens Møller</h1>
-
           <div class="text-center">
-
             <img src="https://www.gravatar.com/avatar/5ce60652703ef30780a3cbb1c0eb0317?s=1000" class="img-fluid center-block" alt="Jens" width="400px">
-
           </div>
-
-        <h3 class="text-center"><a href="#"><i class="fas fa-heart" data-toggle="tooltip" data-placement="top" title="RayRay"></i></a></h3>
-
+        <h3 class="text-center"><i class="fas fa-heart" data-toggle="tooltip" data-placement="top" title="Ray Ray"></i></h3>
         <h3 class="text-center"><a href="https://twitter.com/jensz12"><i class="fab fa-twitter"data-toggle="tooltip" data-placement="top" title="Twitter"></i></a><a href="https://discord.gg/JzpHkvM"><i class="fab fa-discord"data-toggle="tooltip" data-placement="top" title="Discord"></i></a><a href="https://github.com/jensz12"><i class="fab fa-github" data-toggle="tooltip" data-placement="top" title="Github"></i></a><a href="https://instagram.com/jensz12"><i class="fab fa-instagram" data-toggle="tooltip" data-placement="top" title="Instagram"></i></a><a href="https://snapchat.com/add/jensz12"><i class="fab fa-snapchat" data-toggle="tooltip" data-placement="top" title="Snapchat"></i></a></h3>
-
       </div>
-
     </div>
-
   </div>
-
 </div>
-
 <!--<script src="https://static.jensz12.com/script/snowstorm.js"></script>-->
-
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
 <script>
-
 $(function () {
-
 $('[data-toggle="tooltip"]').tooltip()
-
 })
-
 </script>
-
 </body>
-
 </html>
