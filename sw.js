a8405340ca0921bb84eaf62d9458004b534ef6e8
Jens <jens@jensz12.com>
2021-01-16 19:13:04 +0100

    uppdates

--- sw.js
+++ sw.js
@@ -1,38 +1,36 @@
-const cacheName = 'JE-Cache';
-const startPage = 'https://justeat.jensz12.com';
-const offlinePage = 'https://justeat.jensz12.com/';
-const CACHING_DURATION = 7 * 24 * 3600;
+const cacheName = 'jensz12.com-cache';
+const startPage = 'https://jensz12.com';
+const offlinePage = 'https://jensz12.com';
 const filesToCache = [
-	'/img/logo/192.png',
-	'/img/logo/512.png',
-	'/img/logo/1024.png',
-	'/img/logo/je.png',
+  '/favicon.png',
+  '/favicon.ico',
+  '/static/script/snowstorm.js',
+  '/static/design/images/IMG_4034.JPG'
 ];
 
 // Install
-self.addEventListener('install', function (e) {
-	console.log('Juest Eat service worker installation');
+self.addEventListener('install', function(e) {
+	console.log('Jensz12 service worker installation');
 	e.waitUntil(
-		caches.open(cacheName).then(function (cache) {
-			console.log('Just Eat service worker caching dependencies');
-			filesToCache.map(function (url) {
+		caches.open(cacheName).then(function(cache) {
+			console.log('Jensz12 service worker caching dependencies');
+			filesToCache.map(function(url) {
 				return cache.add(url).catch(function (reason) {
-					return console.log('Just Eat: ' + String(reason) + ' ' + url);
+					return console.log('Jensz12: ' + String(reason) + ' ' + url);
 				});
 			});
 		})
 	);
 });
 
-
 // Activate
-self.addEventListener('activate', function (e) {
-	console.log('Just Eat service worker activation');
+self.addEventListener('activate', function(e) {
+	console.log('Jensz12 service worker activation');
 	e.waitUntil(
-		caches.keys().then(function (keyList) {
-			return Promise.all(keyList.map(function (key) {
-				if (key !== cacheName) {
-					console.log('Just Eat old cache removed', key);
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if ( key !== cacheName ) {
+					console.log('Jensz12 old cache removed', key);
 					return caches.delete(key);
 				}
 			}));
@@ -42,48 +40,48 @@ self.addEventListener('activate', function (e) {
 });
 
 // Fetch
-self.addEventListener('fetch', function (e) {
-
+self.addEventListener('fetch', function(e) {
+	
 	// Return if request url protocal isn't http or https
-	if (!e.request.url.match(/^(http|https):\/\//i))
+	if ( ! e.request.url.match(/^(http|https):\/\//i) )
 		return;
-
+	
 	// Return if request url is from an external domain.
-	if (new URL(e.request.url).origin !== location.origin)
+	if ( new URL(e.request.url).origin !== location.origin )
 		return;
-
+	
 	// For POST requests, do not use the cache. Serve offline page if offline.
-	if (e.request.method !== 'GET') {
+	if ( e.request.method !== 'GET' ) {
 		e.respondWith(
-			fetch(e.request).catch(function () {
+			fetch(e.request).catch( function() {
 				return caches.match(offlinePage);
 			})
 		);
 		return;
 	}
-
+	
 	// Revving strategy
-	if (e.request.mode === 'navigate' && navigator.onLine) {
+	if ( e.request.mode === 'navigate' && navigator.onLine ) {
 		e.respondWith(
-			fetch(e.request).then(function (response) {
-				return caches.open(cacheName).then(function (cache) {
+			fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
 					cache.put(e.request, response.clone());
 					return response;
-				});
+				});  
 			})
 		);
 		return;
 	}
 
 	e.respondWith(
-		caches.match(e.request).then(function (response) {
-			return response || fetch(e.request).then(function (response) {
-				return caches.open(cacheName).then(function (cache) {
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
 					cache.put(e.request, response.clone());
 					return response;
-				});
+				});  
 			});
-		}).catch(function () {
+		}).catch(function() {
 			return caches.match(offlinePage);
 		})
 	);
